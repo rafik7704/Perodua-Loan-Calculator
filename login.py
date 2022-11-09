4f8d6669d1db0bf10f435bd1f97bda16798f606b
rafik7704 <rafik7704@gmail.com>
2022-11-09 20:17:32 +0800

    LOGIN UI UPDATE PLC 2.0

--- login.py
+++ login.py
@@ -1,92 +1,206 @@
 from tkinter import *
-#import library
-from time import strftime
+from PIL import ImageTk, Image
 import mysql.connector
-from config import *
-#open databse
-#defining login function
+from time import strftime
+class LoginPage:
+    def __init__(self, window):
+        self.window = window
+        self.window.geometry('1166x718')
+        self.window.resizable(0, 0)
+        self.window.state('zoomed')
+        self.window.title('Login Page')
+        self.window.iconbitmap("perodua.ico")
+        global username
+        global password 
+        global message
+        
+        username=StringVar()
+        password=StringVar()
+        message=StringVar()
+        # ========================================================================
+        # ============================background image============================
+        # ========================================================================
+        self.bg_frame = Image.open('images\\background1.png')
+        photo = ImageTk.PhotoImage(self.bg_frame)
+        self.bg_panel = Label(self.window, image=photo)
+        self.bg_panel.image = photo
+        self.bg_panel.pack(fill='both', expand='yes')
+        # ====== Login Frame =========================
+        self.lgn_frame = Frame(self.window, bg='#040405', width=950, height=600)
+        self.lgn_frame.place(x=200, y=70)
+
+        # ========================================================================
+        # ========================================================
+    
+        # ========================================================================
+        self.txt = strftime('%H:%M %p')
+        self.heading = Label(self.lgn_frame, text=self.txt, font=('yu gothic ui', 25, "bold"), bg="#040405",
+                             fg='white',
+                             bd=5,
+                             relief=FLAT)
+        self.heading.place(x=80, y=30, width=300, height=30)
+        
+       
+        
+        self.welcome="SELAMAT DATANG"
+        self.heading = Label(self.lgn_frame, text=self.welcome, font=('yu gothic ui', 25, "bold"), bg="#040405",
+                             fg='white',
+                             bd=5,
+                             relief=FLAT)
+        self.heading.place(x=80, y=0, width=300, height=30)
+        
+
+        # ========================================================================
+        # ============ Left Side Image ================================================
+        # ========================================================================
+        self.side_image = Image.open('images\\vector.png')
+        photo = ImageTk.PhotoImage(self.side_image)
+        self.side_image_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.side_image_label.image = photo
+        self.side_image_label.place(x=5, y=100)
+
+        # ========================================================================
+        # ============ Sign In Image =============================================
+        # ========================================================================
+        self.sign_in_image = Image.open('images\\hyy.png')
+        photo = ImageTk.PhotoImage(self.sign_in_image)
+        self.sign_in_image_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.sign_in_image_label.image = photo
+        self.sign_in_image_label.place(x=620, y=130)
+
+        # ========================================================================
+        # ============ Sign In label =============================================
+        # ========================================================================
+        self.sign_in_label = Label(self.lgn_frame, text="Sign In", bg="#040405", fg="white",
+                                    font=("yu gothic ui", 17, "bold"))
+        self.sign_in_label.place(x=650, y=240)
+
+        # ========================================================================
+        # ============================username====================================
+        # ========================================================================
+        self.username_label = Label(self.lgn_frame, text="Username", bg="#040405", fg="#4f4e4d",
+                                    font=("yu gothic ui", 13, "bold"))
+        self.username_label.place(x=550, y=300)
+
+        self.username_entry = Entry(self.lgn_frame,textvariable=username,highlightthickness=0, relief=FLAT, bg="#040405", fg="#6b6a69",
+                                    font=("yu gothic ui ", 12, "bold" ))
+        self.username_entry.place(x=580, y=335, width=270)
+
+        self.username_line = Canvas(self.lgn_frame, width=300, height=2.0, bg="#bdb9b1", highlightthickness=0)
+        self.username_line.place(x=550, y=359)
+        # ===== Username icon =========
+        self.username_icon = Image.open('images\\username_icon.png')
+        photo = ImageTk.PhotoImage(self.username_icon)
+        self.username_icon_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.username_icon_label.image = photo
+        self.username_icon_label.place(x=550, y=332)
+
+        # ========================================================================
+        # ============================login button================================
+        # ========================================================================
+        self.lgn_button = Image.open('images\\btn1.png')
+        photo = ImageTk.PhotoImage(self.lgn_button)
+        self.lgn_button_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.lgn_button_label.image = photo
+        self.lgn_button_label.place(x=550, y=450)
+        self.login = Button(self.lgn_button_label, text='LOGIN',command=login, font=("yu gothic ui", 13, "bold"), width=25, bd=0,
+                            bg='#3047ff', cursor='hand2', activebackground='#3047ff', fg='white')
+        self.login.place(x=20, y=10)
+        # ========================================================================
+        # ============================password====================================
+        # ========================================================================
+        self.password_label = Label(self.lgn_frame, text="Password", bg="#040405", fg="#4f4e4d",
+                                    font=("yu gothic ui", 13, "bold"))
+        self.password_label.place(x=550, y=380)
+
+        self.password_entry = Entry(self.lgn_frame, highlightthickness=0,textvariable=password,relief=FLAT, bg="#040405", fg="#6b6a69",
+                                    font=("yu gothic ui", 12, "bold"), show="*")
+        self.password_entry.place(x=580, y=416, width=244)
+
+        self.password_line = Canvas(self.lgn_frame, width=300, height=2.0, bg="#bdb9b1", highlightthickness=0)
+        self.password_line.place(x=550, y=440)
+        # ======== Password icon ================
+        self.password_icon = Image.open('images\\password_icon.png')
+        photo = ImageTk.PhotoImage(self.password_icon)
+        self.password_icon_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.password_icon_label.image = photo
+        self.password_icon_label.place(x=550, y=414)
+        # ========= show/hide password ==================================================================
+        self.show_image = ImageTk.PhotoImage \
+            (file='images\\show.png')
+
+        self.hide_image = ImageTk.PhotoImage \
+            (file='images\\hide.png')
+
+        self.show_button = Button(self.lgn_frame, image=self.show_image, command=self.show, relief=FLAT,
+                                  activebackground="white"
+                                  , borderwidth=0, background="white", cursor="hand2")
+        self.show_button.place(x=860, y=420)
+
+    def show(self):
+        self.hide_button = Button(self.lgn_frame, image=self.hide_image, command=self.hide, relief=FLAT,
+                                  activebackground="white"
+                                  , borderwidth=0, background="white", cursor="hand2")
+        self.hide_button.place(x=860, y=420)
+        self.password_entry.config(show='')
+        Label(self, text="",textvariable=message,bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=900,y=450)
+
+    def hide(self):
+        self.show_button = Button(self.lgn_frame, image=self.show_image, command=self.show, relief=FLAT,
+                                  activebackground="white"
+                                  , borderwidth=0, background="white", cursor="hand2")
+        self.show_button.place(x=860, y=420)
+        self.password_entry.config(show='*')
+    
+
+        
+     
+     
+   
+        
+    
+
+
+
+def meow():
+     global window
+     window = Tk()
+     LoginPage(window)
+     window.mainloop()
+    
+
 def login():
     #getting form data
+    
     connection = mysql.connector.connect(
-      host=hosted,
-      user=user1,
-      password=datapass,
-      port=dataport,
-      database=databased)
+      host='sql12.freesqldatabase.com',
+      user='sql12529996',
+      password='hct9IhwWR2',
+      port='3306',
+      database='sql12529996')
     c = connection.cursor()
     uname=username.get()
     pwd=password.get()
-    actv=activation.get()
    
         #print(c.fetchall())
     
     #applying empty validation
-    if uname=='' or pwd=='' or actv=='':
+    if uname=='' or pwd=='':
         message.set("fill the empty field!!!") 
     else:
       #open database
-      select_query = 'SELECT * FROM `ADMIN` WHERE `USERNAME` = %s and PASSWORD = %s and ACTIVATION = %s' 
-      vals = (uname,pwd,actv)
+      select_query = 'SELECT * FROM `ADMIN` WHERE `USERNAME` = %s and PASSWORD = %s' 
+      vals = (uname,pwd)
       c.execute(select_query, vals)
       user = c.fetchone()
       if user is not None:
        message.set("Login success")
-       login_screen.destroy()
+       window.destroy()
        import database
       else:
        message.set("Wrong username or password!!!")
-#defining loginform function
-
-def Loginform():
-    global login_screen
-    login_screen = Tk()
-    #Setting title of screen
-    login_screen.title("LOG MASUK PERODUA CALCULATOR")
-    login_screen.iconbitmap("images\perodua.ico")
-    #setting height and width of screen
-    login_screen.geometry("350x300")
-    login_screen["bg"]="#1C2833"
-    #declaring variable
     
-    lbl = Label(login_screen,bg="#1C2833",fg="white",font=("Arial",11,"bold"))
-    
-    global  message 
-    global username
-    global password
-    global activation
-    username = StringVar()
-    password = StringVar()
-    message=StringVar()
-    activation=StringVar()
-    #Creating layout of login form
-    Label(login_screen,width="450", text="LOG MASUK ", bg="#0E6655",fg="white",font=("Arial",12,"bold")).pack()
-    #Username Label
-    Label(login_screen, text="Username:",bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=20,y=50)
-    #Username textbox
-    Entry(login_screen, textvariable=username,bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=120,y=52)
-    #Password Label
-    Label(login_screen, text="Password: ",bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=20,y=80)
-    #Password textbox
-    Entry(login_screen, textvariable=password ,show="*",bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=120,y=82)
-     #Password Label
-    Label(login_screen, text="Pin:",bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=20,y=120)
-    #Password textbox
-    Entry(login_screen, textvariable=activation,show="*",bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=120,y=122)
-    #Label for displaying login status[success/failed]
-    Label(login_screen, text="",textvariable=message,bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=70,y=150)
-    #Login button
-    Button(login_screen, text="Login", width=11, height=2, command=login, bg="#0E6655",fg="white",font=("Arial",12,"bold")).place(x=100,y=200)
-   
-    def time():
-     string = strftime('%H:%M:%S %p')
-     lbl.config(text = string)
-     lbl.after(10,time)
-    lbl.pack(anchor = 'sw')
-    
-    time()
-    login_screen.mainloop()
-#calling function Loginform
-Loginform()
 
 
+meow()
 
